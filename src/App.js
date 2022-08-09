dc4bde0a2917d88cf50c6f18d3c2d9f8350d5599
ARIF RUBAYET <72675261+arub9090@users.noreply.github.com>
2022-08-09 12:30:11 -0400

    changing the app. for kuber test v1

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends PureComponent {
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
-        React CICD by ARIF RUBAYET
+        Alhumdulillah
         </Footer>
       </Layout>
     )
